8256874422fedb1a060d47ecf4753aff8b7c7cd4
minal1593 <minalsalunkhe15@gmail.com>
2023-06-18 23:18:46 +0530

    Added : Code to Add New contact in AddressBook

--- src/com/Bridgelabz/AddressBook.java
+++ src/com/Bridgelabz/AddressBook.java
@@ -32,7 +32,7 @@ public class AddressBook implements IContactInfo {
         System.out.print("Enter E-mail:- ");
         String email = input.next();
 
-        Contact contact = new Contact(first , last , add , city , state , zip , phone , email);
+        Contact contact = new Contact(first, last, add, city, state, zip, phone, email);
         contactBook.add(contact);
 
         System.out.println("\nContact added Successfully.");
@@ -40,103 +40,9 @@ public class AddressBook implements IContactInfo {
 
     public void DisplayContacts() {
         System.out.println("\nContacts Present in Address Book:");
-        for(Contact  person : contactBook) {
+        for (Contact person : contactBook) {
             System.out.println(person);
         }
     }
 
-    public void EditContact() {
-        System.out.println("\nEnter first name to edit :- ");
-        String name = input.next();
-
-        for (Contact person : contactBook) {
-            if (name.equals(person.First_Name)) {
-                System.out.print("\nSelect option to edit----\n1.First_name \n2.Last_name \n3.Address \n4.City \n5.State \n6.Zip_code \n7.Phone_number \n8.Email :- ");
-                int option = input.nextInt();
-                switch (option) {
-                    case 1:
-                        System.out.print("Enter new first name :- ");
-                        String newFirstName = input.next();
-                        person.setFirst_Name(newFirstName);
-                        System.out.println("First name is updated.");
-                        break;
-
-                    case 2:
-                        System.out.print("Enter new last name :- ");
-                        String newLastName = input.next();
-                        person.setLast_Name(newLastName);
-                        System.out.println("Last name is updated.");
-                        break;
-
-                    case 3:
-                        System.out.print("Enter new Address :- ");
-                        String newAddress = input.next();
-                        person.setAddress(newAddress);
-                        System.out.println("Address is updated.");
-                        break;
-
-                    case 4:
-                        System.out.print("Enter new city name :- ");
-                        String newCity = input.next();
-                        person.setCity(newCity);
-                        System.out.println("City is updated.");
-                        break;
-
-                    case 5:
-                        System.out.print("Enter new state name :- ");
-                        String newState = input.next();
-                        person.setState(newState);
-                        System.out.println("State is updated.");
-                        break;
-
-                    case 6:
-                        System.out.print("Enter new Zip code :- ");
-                        int newZip = input.nextInt();
-                        person.setZip_code(newZip);
-                        System.out.println("Zip code is updated.");
-                        break;
-
-                    case 7:
-                        System.out.print("Enter new phonr number :- ");
-                        int newPhone = input.nextInt();
-                        person.setPhone_number(newPhone);
-                        System.out.println("Phone number is updated.");
-                        break;
-
-                    case 8:
-                        System.out.print("Enter new email :- ");
-                        String newEmail = input.next();
-                        person.setEmail(newEmail);
-                        System.out.println("Email is updated.");
-                        break;
-
-                    default:
-                        System.out.println("Please enter a number between 1 to 8 only...");
-
-                }
-            }
-            else {
-                continue;
-
-            }
-        }
-    }
-
-    public void DeleteContact() {
-        System.out.print("Enter first name to delete contact:- ");
-        String name_to_delete = input.next();
-
-        for (int i = 0; i < contactBook.size(); i++) {
-            String search_name_in_arrayList = contactBook.get(i).getFirst_Name();
-
-            if (name_to_delete.equals(search_name_in_arrayList)) {
-                contactBook.remove(i);
-                System.out.println("\nEntered contact deleted successfully.");
-                break;
-            } else {
-                continue;
-
-            }
-        }
-    }
 }
--- src/com/Bridgelabz/IContactInfo.java
+++ src/com/Bridgelabz/IContactInfo.java
@@ -2,7 +2,4 @@ package com.Bridgelabz;
 
 public interface IContactInfo {
     void AddContact();
-    void EditContact();
-    void DeleteContact();
-    void DisplayContacts();
 }
